bc00bc4909ca05f105149e4ffc25b017c911781a
Manuela-AYO <75300081+Manuela-AYO@users.noreply.github.com>
2025-04-24 18:24:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,56 @@
-import os
-import getpass
-from dotenv import load_dotenv
-
-from langsmith import utils
-from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_groq import ChatGroq
-from langchain_core.messages import HumanMessage
-from langgraph.prebuilt import create_react_agent
-from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader, YoutubeLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langsmith import utils
+import validators
 
-load_dotenv()
-os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGSMITH_PROJECT")
+load_dotenv(dotenv_path="../.env")
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "summarizer"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGSMITH_TRACING")
-os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
-groq_api_key = os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
-is_tracing_enabled = utils.tracing_is_enabled()
+enabled_tracing = utils.tracing_is_enabled()
 
-if is_tracing_enabled:
-    # init the research tool
-    search = TavilySearchResults(max_results=2)
+model = ChatGroq(model_name="Llama3-8b-8192")
+map_prompt = ChatPromptTemplate.from_messages([
+    ("system", "Make a concise summary of {text}")
+])
+combine_prompt = ChatPromptTemplate.from_messages([
+    ("system", "Using each of the summary provided, make an overall summary {text}")
+])
 
-    # add it to the list of tools
-    tools = [search]
+chain = load_summarize_chain(llm=model, chain_type="map_reduce", map_prompt=map_prompt, combine_prompt=combine_prompt)
 
-    # memory
-    memory = MemorySaver()
-    config = { "configurable": {"thread_id": "chat1"} }
 
-    # model
-    model = ChatGroq(model_name="Deepseek-R1-Distill-Qwen-32b", groq_api_key=groq_api_key)
-    
-    # bind with the research tool
-    # model_with_tools = model.bind_tools(tools)
+st.title("Youtube video and website summarizer")
 
-    # create the agent which will under the hood bind_tools to the model
-    agent_executor = create_react_agent(model, tools, checkpointer=memory)
-
-    # streaming
-    for step in agent_executor.stream({"messages": [HumanMessage(content="Hi i'm manuela")]}, config=config):
-        print(step)
-        print("----------")
-
-    print("******************")
-    for step in agent_executor.stream({"messages": [HumanMessage(content="what's my name")]}, config=config):
-        print(step)
-        print("----------")
+if enabled_tracing:
+    with st.container():
+        site_url = st.text_input(label="Youtube or website link", placeholder="Enter a valid Youtube or website link")
+        if st.button(label="Valider", type="primary"):
+            site_url = site_url.strip()
+            url_validator = validators.url(site_url)
+            if url_validator:
+                try:
+                    docs = []
+                    if "youtube.com" in site_url:
+                        docs = YoutubeLoader.from_youtube_url(site_url).load()
+                    else:
+                        docs = WebBaseLoader(site_url).load()
+                    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+                    documents = text_splitter.split_documents(docs)
+                    # st.text("Your awesome summary is being prepared...")
+                    summary = chain.run(documents)
+                    st.text("Your summary")
+                    st.text(summary)
+                except Exception as e:
+                    st.error("An error occured ", e)
+            else: 
+                    st.error("This is not a valid url")
